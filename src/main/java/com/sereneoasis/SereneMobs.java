e05c7bfee9a5ad861df23cc452a00de3e1c21adf
Sujan Naik <sujannaik047@gmail.com>
2024-09-18 15:00:17 +0100

    Initialise project

---
+++ src/main/java/com/sereneoasis/SereneMobs.java
@@ -0,0 +1,20 @@
+package com.sereneoasis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SereneMobs extends JavaPlugin {
+
+    public static SereneMobs plugin;
+
+    @Override
+    public void onEnable(){
+        plugin = this;
+
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
